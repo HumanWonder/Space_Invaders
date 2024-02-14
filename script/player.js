e8da88d233ff039d83e60a4c60273e655e0e9701
afouquem <axellefouq@hotmail.fr>
2024-02-14 14:57:46 +0100

    added comments

--- script/player.js
+++ script/player.js
@@ -28,29 +28,41 @@ function moveRight() {
         player.style.left = leftBorder + speed + "px";
     }
 }
+//makes the missile div on key press
 function shoot() {
     console.log("Pew");
-    
+    //gets the x position of the player by its style (applied before)
     var playerXPos = parseInt((getComputedStyle(player).left).split('px')[0]);
     // var playerYPos = playerStartYPos;
+    //creates the div for the missile with an id (to apply style and find it later)
     const missile = document.createElement("div");
     missile.id = "missile";
 
+    //where the missile will be placed on screen
     playerXPos += playerWidth/2;
     missile.style.left = playerXPos + "px";
     missile.style.top = (playerStartYPos-playerHeight) + "px";
+    //only appends the div once with a bool (it's false again after a condition in animateMissile func)
     if (!missileFired) {
         workspace.appendChild(missile);
+        //will prevent shoot() to append missiles when space is pressed
         missileFired = true;
     }
 }
+//moves the missile upwards. The func is called from animate func in gameManager.js
 function animateMissile() {
+    //gets the top value
     var missilePos = ((getComputedStyle(missile).top).split("px")[0]);
+    //gets the div to reapply values
     var div = document.getElementById("missile");
     missilePos -= speed;
+    //reapplies top value to make it move a bit more up
     div.style.top = missilePos + "px";
+    //condition to stop the missile (here where it reaches the top of the workspace div)
     if (missilePos <= 0) {
+        //remove the div
         missile.remove();
+        //bool to make shoot() able to recreate a missile on command
         missileFired = false;
     }
 }
@@ -60,6 +72,7 @@ document.addEventListener("keydown", event => {
         console.log("pressed on")
         controller[event.keyCode].pressed = true
     }
+    //launches the function on key press
     if (event.key == " ") {
         shoot();
     }
